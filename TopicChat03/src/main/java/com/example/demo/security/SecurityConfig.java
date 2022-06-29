57194cd18196764b1b65931b31b0805a5562ee84
ryo <sss_doggy@yahoo.co.jp>
2022-06-29 17:28:00 +0900

    index.htmlのsecurity許可

--- TopicChat03/src/main/java/com/example/demo/security/SecurityConfig.java
+++ TopicChat03/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-        .antMatchers("/signup").permitAll()
+        .antMatchers("/index").permitAll()
         .anyRequest().authenticated().and()
         .formLogin().loginPage("/login").defaultSuccessUrl("/group/group").permitAll().and()
         .logout().logoutSuccessUrl("/login").permitAll();
