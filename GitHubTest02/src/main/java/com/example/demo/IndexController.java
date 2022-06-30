2ccb3f897da9d2e42e594d98efbfdc5ae276c761
ryo <sss_doggy@yahoo.co.jp>
2022-06-30 13:19:14 +0900

    IndexControllerの追加

---
+++ GitHubTest02/src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	@GetMapping("/")
+	public String index() {
+		return "/index";
+	}
+	
+	@GetMapping("/index")
+	public String getIndex() {
+		return "/index";
+	}
+}
